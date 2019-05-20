8d09eb11d3f2a4bbb122a105f7d1ada73152e823
Gil Greenberg <ggreenberg@somethingdigital.com>
2019-05-19 20:32:18 -0400

    Add hide icon and add new view icon to match

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,6 +28,14 @@ import PageLayout from "../components/layout";
 import SEO from "../components/seo";
 import GraphqlProvider from "../components/graphqlProvider";
 
+const hideIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16"><g transform="translate(0, 0)"><path fill="#637381" d="M14.574,5.669l-1.424,1.424C13.428,7.44,13.656,7.757,13.819,8c-0.76,1.13-2.85,3.82-5.561,3.985L6.443,13.8 C6.939,13.924,7.457,14,8,14c4.707,0,7.744-5.284,7.871-5.508c0.171-0.304,0.172-0.676,0.001-0.98 C15.825,7.427,15.372,6.631,14.574,5.669z"></path> <path fill="#637381" d="M0.293,15.707C0.488,15.902,0.744,16,1,16s0.512-0.098,0.707-0.293l14-14c0.391-0.391,0.391-1.023,0-1.414 s-1.023-0.391-1.414,0l-2.745,2.745C10.515,2.431,9.331,2,8,2C3.245,2,0.251,7.289,0.126,7.514 c-0.169,0.303-0.168,0.672,0.002,0.975c0.07,0.125,1.044,1.802,2.693,3.276l-2.529,2.529C-0.098,14.684-0.098,15.316,0.293,15.707z M2.181,7.999C2.958,6.835,5.146,4,8,4c0.742,0,1.437,0.201,2.078,0.508L8.512,6.074C8.348,6.029,8.178,6,8,6C6.895,6,6,6.895,6,8 c0,0.178,0.029,0.348,0.074,0.512L4.24,10.346C3.285,9.51,2.559,8.562,2.181,7.999z"></path></g></svg>
+)
+
+const viewIcon = (
+  <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16"><g transform="translate(0, 0)"><path fill="#637381" d="M8,14c4.707,0,7.744-5.284,7.871-5.508c0.171-0.304,0.172-0.676,0.001-0.98C15.746,7.287,12.731,2,8,2 C3.245,2,0.251,7.289,0.126,7.514c-0.169,0.303-0.168,0.672,0.002,0.975C0.254,8.713,3.269,14,8,14z M8,4 c2.839,0,5.036,2.835,5.818,4C13.034,9.166,10.837,12,8,12c-2.841,0-5.038-2.838-5.819-4.001C2.958,6.835,5.146,4,8,4z"></path> <circle data-color="color-2" fill="#637381" cx="8" cy="8" r="2"></circle></g></svg>
+)
+
 const orderData = {
   email: faker.internet.email(),
   billingAddress: {
@@ -268,7 +276,7 @@ class IndexPage extends React.Component {
                       helpText="We do not collect or store this information."
                       connectedRight={
                         <Button
-                          icon="view"
+                          icon={this.state.isPasswordVisible ? hideIcon : viewIcon}
                           label="Toggle Password"
                           onClick={this.handlePasswordToggle}
                         />
